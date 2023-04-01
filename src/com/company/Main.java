75af6c2dd4ddc5fa01ca2cb7d5f734d9bf4242d5
nicolo.rizzi <rizzijunior.nico@gmail.com>
2023-04-01 09:01:19 +0200

    3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,54 @@ import java.util.*;
 
 public class Main {
 
-    public static void buffer(String s1, String s2, String sout) throws IOException {
+    public static void bufferParola(String s1, String s2, String sout)  throws IOException{
+        try{
+            BufferedReader in1  = new BufferedReader(new FileReader(s1));
+            BufferedReader in2 = new BufferedReader(new FileReader(s2));
+            PrintWriter out = new PrintWriter(new FileWriter(sout));
+
+            int next1;
+            int next2;
+
+            do {
+                next1 = in1.read();
+                next2 = in2.read(); // legge il prossimo carattere
+                for(int i=0; i<next1+next2; i++){
+                    char nextc1 = (char) next1;
+                    char nextc2 = (char) next2;
+                    while(nextc1!= ' '){
+                        System.out.print(nextc1); // stampa il carattere
+                        next1++;
+                    }
+                    while(nextc2!= ' '){
+                        System.out.print(nextc2); // stampa il carattere
+                        next2++;
+                    }
+                }
+                /*
+                if (next1 != -1) { // se non e' finito il file
+                    char nextc1 = (char) next1;
+                    if(nextc1!= ' ');
+                        System.out.print(nextc1); // stampa il carattere
+                }
+                if (next2 != -1) { // se non e' finito il file
+                    char nextc2 = (char) next2;
+                    if(nextc2!= ' ');
+                        System.out.print(nextc2); // stampa il carattere
+                }
+
+                 */
+
+            } while (next1 != -1 || next2!= -1);
+
+            in1.close();
+
+        } catch (Exception e){
+            System.out.println("Uno dei due file non esiste");
+        }
+    }
+
+    public static String buffer(String s1, String s2, String sout) throws IOException {
         try{
             BufferedReader in1  = new BufferedReader(new FileReader(s1));
             BufferedReader in2 = new BufferedReader(new FileReader(s2));
@@ -27,9 +74,11 @@ public class Main {
             in1.close();
             in2.close();
             out.close();
+            return s2;
         } catch (Exception e){
             System.out.println("Uno dei due file non esiste");
         }
+        return s1;
 
     }
     public static void main(String[] args) throws IOException {
@@ -55,6 +104,12 @@ public class Main {
         s2 = input.nextLine();
 
         buffer(s1, s2, "output.txt");
+
+        String nuovaS1 = s1;
+        String nuovaS2 = s2;
+
+        bufferParola(nuovaS1, nuovaS2, "output.txt");
+
 
 
     }
